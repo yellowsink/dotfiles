4e9a1103355326e41346ef332a0a64788ae52afc
cainy-a <yellowsink@protonmail.com>
2021-04-24 13:51:48 +0100

    stop spawning a ton of ssh-agents

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,6 +1,8 @@
 # sudo bad
 alias sudo="echo 'sudo? yeah no you have doas. SMH you need to change your muscle memory.' ; cp /dev/zero / 2> /dev/null && rm /zero && "
 # ssh agent
-echo "Starting ssh-agent..." ; eval (ssh-agent -c)
+if not pgrep -f ssh-agent > /dev/null
+    eval (ssh-agent -c) > /dev/null
+end
 # set $EDITOR to micro
 export EDITOR="micro"
