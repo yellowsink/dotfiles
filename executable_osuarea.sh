7b96818eeaa7abe3b6bac67fe352f2cf6e2c2a0e
Cain Atkinson <yellowsink@protonmail.com>
2021-08-05 23:01:16 +0100

    tablet area, some pipe fixes

--- executable_osuarea.sh
+++ executable_osuarea.sh
@@ -1,9 +1,10 @@
 # Set tablet area
 # this is calculated for a CTL460 to
-# have a 32.5x18.2mm area (almost) perfectly centered
-# (i needed to fix my ratio and couldnt be bothered to re-center it),
+# have a 36mm wide area at 16:9, perfectly centered
 # and the tablet will be rotated clockwise 90 degrees
 # and mapped to the HDMI-A-0 display
 xsetwacom set "Wacom Bamboo Pen Pen stylus" MapTooutput HDMI-A-0 2> /dev/null
-xsetwacom set "Wacom Bamboo Pen Pen stylus" Area 6390 2975 8210 6225
+xsetwacom set "Wacom Bamboo Pen Pen stylus" Area 5560 2012 7585 5612
 xsetwacom set "Wacom Bamboo Pen Pen stylus" Rotate ccw
+
+# calculations done automatically by https://github.com/yellowsink/WacomAreaX11
