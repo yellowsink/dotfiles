d06013e2d24c32d6cfada332fca2c15e066e595b
yellowsink <yellowsink@riseup.net>
2022-09-10 12:36:23 +0100

    goodbye st_ink, it was nice but your kerning died for ?? reason, hello wezterm!

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require "wezterm"
+
+return {
+	font = wezterm.font "Cozette",
+	font_size = 13,
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+	window_background_opacity    = 0.85,
+}
