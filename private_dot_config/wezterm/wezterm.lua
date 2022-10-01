c41dc2ae4d28b4ea5b35bed405a8a6a9c94bd0e3
yellowsink <yellowsink@riseup.net>
2022-10-01 11:45:27 +0100

    cursor

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require "wezterm"
 
 return {
-	font = wezterm.font "Cozette",
-	font_size = 13,
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	window_background_opacity    = 0.85,
+	font                      = wezterm.font "Cozette",
+	font_size                 = 13,
+	color_scheme              = "Catppuccin Mocha",
+	enable_tab_bar            = false,
+	window_background_opacity = 0.85,
+	xcursor_theme             = "Vimix-white-cursors"
 }
