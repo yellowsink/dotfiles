49773ae22e063c84775fd00b08b22b01cf9aee89
yellowsink <yellowsink@riseup.net>
2023-02-15 16:08:46 +0000

    make config cross-machine

--- private_dot_config/nixpkgs/home.nix
+++ private_dot_config/nixpkgs/home.nix
@@ -3,8 +3,8 @@
 {
 	# Home Manager needs a bit of information about you and the
 	# paths it should manage.
-	home.username = "cain";
-	home.homeDirectory = "/home/cain";
+	home.username = builtins.getEnv "USER";
+	home.homeDirectory = builtins.getEnv "HOME";
 
 	# This value determines the Home Manager release that your
 	# configuration is compatible with. This helps avoid breakage
